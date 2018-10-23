8b30416aa24f840446f62c4fbeea9c4f2e4b8c13
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-10-22 22:05:55 -0500

    finished removing unnecessary data from recent crimes

--- data_parser/get_useful_data.py
+++ data_parser/get_useful_data.py
@@ -1,5 +1,47 @@
 import pandas as pd
 
-def get_useful_data(filename, danger_zones):
+def crime_type_formatter(tp):
+    """
+    Cleans up the description given by the UIPD.
+    i.e. UIPD description data is given as '[UIPD] UNLAWFUL USE OF WEAPONS'
+    This method truncates it such that it becomes 'Unlawful use of weapons'
+
+    :return: truncated, well-formatted description
+    """
+
+    if "[UIPD]" in tp:
+        tp = tp[7:]
+
+    return tp.capitalize()
+
+def get_useful_data(filename):
     """ Retrieves the information that the front-end needs to map and display the crime zones. Saves data as CSV. """
-    pass
+    recent_severe_crimes = pd.read_csv(filename)
+
+    #important data for recent crimes:
+    # - crime location (coordinates for map use only)
+    # - crime time
+    # - crime type
+    # - crime description
+    # - address (for human use only)
+
+    important_recent = ("latitude","longitude","incident_datetime","incident_type_primary","incident_description",
+                        "address_1","address_2","city","state","zip")
+
+    for label in list(recent_severe_crimes.columns.values):
+        if label not in important_recent:
+            recent_severe_crimes = recent_severe_crimes.drop(columns=label)
+
+    #important data for danger zones
+    # - crime location
+    # - frequency (emphasize the time period)
+
+    important_frequent = ("latitude","longitude")
+
+
+def main():
+    get_useful_data("filtered_crime_data.csv")
+
+
+if __name__== "__main__":
+    main()
