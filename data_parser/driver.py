6591699dc77f3b6edbe6bb888c414054878e8622
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-10-22 19:51:20 -0500

    new file to get most important data for frontend

--- data_parser/driver.py
+++ data_parser/driver.py
@@ -1,11 +1,14 @@
 from update_csv import *
 from filter_by_type import *
 from filter_by_time import *
+from emphasis_frequency import *
 
 def main():
     update_csv()
     filter_types("data_parser\\u_of_i_crime_data.csv")
     filter_old_data("data_parser\\filtered_crime_data.csv")
+    danger_zones = frequency_counter("data_parser\\u_of_i_crime_data.csv")
+
 
 if __name__== "__main__":
     main()
---
+++ data_parser/get_useful_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def get_useful_data(filename, danger_zones):
+    """ Retrieves the information that the front-end needs to map and display the crime zones. Saves data as CSV. """
+    pass
