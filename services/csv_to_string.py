87900efb40080612642cb7bb8c693d047d1ded4d
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-12-07 01:30:04 -0600

    adding severity processing for csv to string

--- services/csv_to_string.py
+++ services/csv_to_string.py
@@ -10,4 +10,7 @@ def csv_to_string():
     with open("services/frontend_files/recent_severe_crimes.csv") as f:
         recent_string = f.read() + "\n"
 
-    return danger_string, recent_string
+    with open("services/frontend_files/severity_and_avoidance_radius.csv") as f:
+        severity_string = f.read() + "\n"
+
+    return danger_string, recent_string, severity_string
